71312c547094a5177545990f0141e1329cfdec8d
Ingo Bürk <ingo.buerk@tngtech.com>
2015-04-06 14:00:31 +0200

    combined commands

--- .config/i3lock/i3lock.sh
+++ .config/i3lock/i3lock.sh
@@ -2,14 +2,14 @@
 set -eu
 
 screenshot=$(mktemp --tmpdir i3lock-scrot-XXX.png)
-blurred=$(mktemp --tmpdir i3lock-blurred-XXX.png)
 lock=$(mktemp --tmpdir i3lock-final-XXX.png)
 overlay="${HOME}/.config/i3lock/overlay.png"
 
 scrot -d0 ${screenshot}
-convert ${screenshot} -blur 0x5 -modulate 100,60 ${blurred}
-convert -gravity center -composite ${blurred} ${overlay} ${lock}
+convert ${screenshot} -blur 0x5 -modulate 100,40 \
+    -gravity center ${overlay} -composite \
+    ${lock}
 
 i3lock -u -i ${lock}
 
-trap "rm '${screenshot}' '${blurred}' '${lock}'" EXIT
+trap "rm '${screenshot}' '${lock}'" EXIT
