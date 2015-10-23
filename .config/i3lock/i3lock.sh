05053e02d68c11b3f94b196d987f7f35f20a4166
Ingo Bürk <ingo.buerk@tngtech.com>
2015-10-24 00:55:01 +0200

    Move lock on suspend to systemd

--- .config/i3lock/i3lock.sh
+++ .config/i3lock/i3lock.sh
@@ -6,11 +6,15 @@ lock=$(mktemp --tmpdir i3lock-final-XXX.png)
 overlay="${HOME}/.config/i3lock/overlay.png"
 
 scrot -d0 ${screenshot}
+
+# lock initially to compensate for the delay
+i3lock -u -c 000000
+
 convert ${screenshot} -blur 0x5 -modulate 100,40 \
     -gravity center ${overlay} -composite \
     ${lock}
 
+killall i3lock
 i3lock -n -u -i ${lock}
 
-#trap "rm '${screenshot}' '${lock}'" EXIT
 rm '${screenshot}' '${lock}'
