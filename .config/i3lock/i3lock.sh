8ec00873434c01d5adf73227262a53a44ffd182e
Ingo Bürk <ingo.buerk@tngtech.com>
2015-10-27 10:17:56 +0100

    lock stuff

--- .config/i3lock/i3lock.sh
+++ .config/i3lock/i3lock.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 set -eu
+#i3lock -n -u -t -i ${HOME}/.config/wallpapers/space.png
+i3lock -n -u -t -i ${HOME}/.config/i3lock/lock.png
 
-screenshot=$(mktemp --tmpdir i3lock-scrot-XXX.png)
-lock=$(mktemp --tmpdir i3lock-final-XXX.png)
-overlay="${HOME}/.config/i3lock/overlay.png"
+#screenshot=$(mktemp --tmpdir i3lock-scrot-XXX.png)
+#lock=$(mktemp --tmpdir i3lock-final-XXX.png)
+#overlay="${HOME}/.config/i3lock/overlay.png"
 
-scrot -d0 ${screenshot}
+#scrot -d0 ${screenshot}
 
 # lock initially to compensate for the delay
-i3lock -u -c 000000
+#i3lock -u -c 000000
 
-convert ${screenshot} -blur 0x5 -modulate 100,40 \
-    -gravity center ${overlay} -composite \
-    ${lock}
+#convert ${screenshot} -blur 0x5 -modulate 100,40 \
+#    -gravity center ${overlay} -composite \
+#    ${lock}
 
-killall i3lock
-i3lock -n -u -i ${lock}
+#killall i3lock
+#i3lock -n -u -i ${lock}
 
-rm '${screenshot}' '${lock}'
+#rm '${screenshot}' '${lock}'
