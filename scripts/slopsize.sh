5816d3e8bb044de4403d908498a8d2596288940e
Ingo Bürk <ingo.buerk@tngtech.com>
2016-03-06 13:04:25 +0100

    slopsize: float window

--- scripts/slopsize.sh
+++ scripts/slopsize.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eu
 
-eval $(slop -b 3 -c 0.96,0.5,0.09 -t 0) && xdotool getactivewindow windowmove $X $Y && xdotool getactivewindow windowsize $W $H
+eval $(slop -b 3 -c 0.96,0.5,0.09 -t 0) && i3 floating enable && xdotool getactivewindow windowmove $X $Y && xdotool getactivewindow windowsize $W $H
