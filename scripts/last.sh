c72fce75a8794102fcf8349ec738cc362ab5419e
Ingo Bürk <ingo.buerk@tngtech.com>
2016-04-06 19:10:24 +0200

    last.sh

--- scripts/last.sh
+++ scripts/last.sh
@@ -6,5 +6,6 @@ do
     read line
 
     [[ -z "$last" ]] || i3-msg "[id=$last] mark last"
-    last=$(echo "$line" | awk -F' ' '{printf $NF}')
+    _last=$(echo "$line" | awk -F' ' '{printf $NF}')
+    [[ "$_last" = "0x0" ]] || last="$_last"
 done
