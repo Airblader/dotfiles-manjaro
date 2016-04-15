05d136b0440559699fcf6f42e572848f6867309d
Ingo Bürk <ingo.buerk@tngtech.com>
2016-04-15 20:44:47 +0200

    fix script

--- scripts/last.sh
+++ scripts/last.sh
@@ -5,7 +5,7 @@ xprop -root -spy _NET_ACTIVE_WINDOW | while :
 do
     read line
 
-    [[ -z "$last" ]] || i3-msg "[id=$last] mark last"
+    [[ -z "$last" ]] || i3-msg "[id=$last] mark --add last"
     _last=$(echo "$line" | awk -F' ' '{printf $NF}')
     [[ "$_last" = "0x0" ]] || last="$_last"
 done
