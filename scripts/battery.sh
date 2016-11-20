a74a62a77674d261dc9e602aa363a72adb86eb29
Ingo Bürk <ingo.buerk@tngtech.com>
2016-11-20 01:13:41 +0100

    battery script fixes

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -12,10 +12,10 @@ i3status -c $HOME/.config/i3blocks/i3status.conf | while IFS= read -r line; do
     STATE=""
     [[ "${PARTS[0]}" = "BAT" ]] && {
         STATE="${PARTS[2]}"
-        [[ -z "${STATE}" ]] && STATE="??:??:00"
-
-        STATE="${STATE:0:-3}"
-        STATE=" (${STATE})"
+        [[ -n "${STATE}" ]] && {
+            STATE="${STATE:0:-3}"
+            STATE=" (${STATE})"
+        }
     }
 
     LEVEL=$(( (CHARGE - 1) / 20 ))
@@ -24,6 +24,6 @@ i3status -c $HOME/.config/i3blocks/i3status.conf | while IFS= read -r line; do
     echo -e "  \u${ICON}  ${CHARGE}${STATE} "
 
     [[ "${LEVEL}" = "0" ]] && {
-        [[ -z "${STATE}" ]] || i3-msg "fullscreen disable"
+        [[ -z "${STATE}" ]] || i3-msg "fullscreen disable" >/dev/null
     }
 done
