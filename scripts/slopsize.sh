5f4866027b8bb0d3be431d9fed723e9d804604fd
Ingo Bürk <ingo.buerk@tngtech.com>
2017-03-02 14:08:08 +0100

    fix script

--- scripts/slopsize.sh
+++ scripts/slopsize.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -eu
 
-eval $(slop -b 3 -c 0.96,0.5,0.09 -t 0) && i3 floating enable && xdotool getactivewindow windowmove $X $Y && xdotool getactivewindow windowsize $W $H
+eval $(slop -b 3 -c 0.96,0.5,0.09 -t 0 -f "X=%x Y=%y W=%w H=%h")
+i3 floating enable && xdotool getactivewindow windowmove $X $Y && xdotool getactivewindow windowsize $W $H
