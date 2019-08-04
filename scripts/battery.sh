0b6dff896b2959441c2414257e5beda7fbdbf31a
Ingo Bürk <admin@airblader.de>
2019-08-04 10:12:45 +0200

    spacing

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -21,7 +21,7 @@ i3status -c $HOME/.config/i3blocks/i3status.conf | while IFS= read -r line; do
     LEVEL=$(( (CHARGE - 1) / 20 ))
     ICON="f$(( 244 - LEVEL ))"
 
-    echo -e "  \u${ICON}  ${CHARGE}${STATE} "
+    echo -e " \u${ICON} ${CHARGE}${STATE} "
 
     [[ "${LEVEL}" = "0" ]] && {
         [[ -z "${STATE}" ]] || i3-msg "fullscreen disable" >/dev/null
